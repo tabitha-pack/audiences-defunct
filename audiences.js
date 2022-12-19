e91492816016b2935657faeeba9f543454c4f3ef
tabitha-pack <121058837+tabitha-pack@users.noreply.github.com>
2022-12-19 12:12:53 -0800

    Create audiences.js

---
+++ audiences.js
@@ -0,0 +1,152 @@
+function main(){
+  
+   function percentageAdjustment(audienceBiddingField){
+       /perform bid adjustments by % on audience segments/
+       var oldBid = audienceBiddingField.getBidModifier(); 
+       var newBid = oldBid - .10;
+       audienceBiddingField.setBidModifier(newBid);
+       console.log(audienceBiddingField.getBidModifier());  
+    }
+    
+    function binaryAdjustment(theAudience){
+      //perform enable or exclude
+      console.log(theAudience);
+      theAudience.remove();
+      console.log(theAudience.isEnabled());
+    }
+    
+  const percentIncrease = 50; 
+  const minClicks = 20; 
+  const minConversions = 5;
+   
+  var campaignSelector = AdsApp
+      .campaigns()
+      .forDateRange("ALL_TIME");
+  var campaignIterator = campaignSelector.get();
+  
+      while(campaignIterator.hasNext()){
+        var campaign = campaignIterator.next(); 
+        var activeCampaign = campaign.isEnabled();
+        var baseCampaign = campaign.isBaseCampaign();
+      
+         if (activeCampaign && baseCampaign) {  
+           var campaignName = campaign.getName();
+           var biddingType = campaign.getBiddingStrategyType();
+           var campaignAudiences = campaign.targeting().audiences().get();
+           var audiencesCount = campaignAudiences.totalNumEntities();
+      
+          if(audiencesCount > 0){
+            var stats = campaign.getStatsFor("ALL_TIME");
+            var conversions = stats.getConversions();   
+            var cost = stats.getCost();
+            var cCostConv = cost/conversions; 
+            console.log('campaign cost/conv:' + cCostConv);
+        
+              while (campaignAudiences.hasNext()) {
+                var audience = campaignAudiences.next();                         
+                var biddingField = audience.bidding();
+                var currentBid = biddingField.getBidModifier();
+                console.log('Campaign:' + campaignName + ' Audience: ' + audience.getName() + ' ' + currentBid);          
+                var audienceStats = audience.getStatsFor("ALL_TIME");
+                var audienceCost = audienceStats.getCost(); 
+                var audienceConversions = audienceStats.getConversions(); 
+                var audienceClicks = audienceStats.getClicks(); 
+                 if (audienceConversions > minConversions && audienceClicks > minClicks){
+                   var audienceCostConv = audienceCost/audienceConversions;
+                   var difference = audienceCostConv - cCostConv;
+                   var increase = (difference/cCostConv) * 100;
+                   /*change to increase >= percentIncrease for live implementation*/
+                    if(increase >= percentIncrease){
+                     console.log(increase);
+                       if(biddingType === 'MAXIMIZE_CONVERSIONS'){
+                         binaryAdjustment(audience); 
+                       }
+                       if(biddingType === 'TARGET_SPEND'){
+                         var modifyBid = audience.bidding();
+                         percentageAdjustment(modifyBid); 
+                        }
+                     }
+                   }else if (audienceConversions == 0 && audienceClicks > minClicks) {
+                     var difference = audienceCost - cCostConv; 
+                     var increase = (difference/cCostConv) * 100;
+                     /*change to increase >= percentIncrease for live implementation*/
+                      if(increase >= percentIncrease){
+                       console.log(increase);
+                         if(biddingType === 'MAXIMIZE_CONVERSIONS'){                                        
+                           binaryAdjustment(audience); 
+                         }
+                         if(biddingType === 'TARGET_SPEND'){        
+                           var modifyBid = audience.bidding();
+                           percentageAdjustment(modifyBid); 
+                         }
+                     }  
+                 } /*audience conversions = 0 loop */
+              }/*campaign audiences loop*/
+        
+          }else {
+             var adGroupSelector = AdsApp
+              .adGroups()
+              .forDateRange("ALL_TIME");
+             var adGroupIterator = adGroupSelector.get(); 
+               while(adGroupIterator.hasNext()){
+                var adGroup = adGroupIterator.next();
+                var activeAdGroup = adGroup.isEnabled();
+                  if (activeAdGroup) {
+                   var adGroupCampaign = adGroup.getCampaign();
+                   var adGroupCampaignName = adGroupCampaign.getName();
+                   var adGroupName = adGroup.getName();
+                    if(campaignName == adGroupCampaignName){
+                    
+                      var adGroupAudienceSelector = adGroup.targeting()
+                        .audiences()
+                        .forDateRange("ALL_TIME");
+                       
+                      var audienceIterator = adGroupAudienceSelector.get();
+                        while (audienceIterator.hasNext()) {
+                          var audience = audienceIterator.next();                             
+                          var biddingField = audience.bidding();
+                          var currentBid = biddingField.getBidModifier();
+                          console.log('adGroup Name: ' + adGroupName + 'Audience Name: ' + audience.getName() + ' ' + currentBid);                       
+                          var audienceStats = audience.getStatsFor("ALL_TIME");
+                          var audienceCost = audienceStats.getCost(); 
+                          var audienceConversions = audienceStats.getConversions(); 
+                          var audienceClicks = audienceStats.getClicks(); 
+                            if (audienceConversions > minConversions && audienceClicks > minClicks){
+                             var audienceCostConv = audienceCost/audienceConversions;
+                             var difference = audienceCostConv - cCostConv;
+                             var increase = (difference/cCostConv) * 100;
+                             /*change to increase >= percentIncrease for live implementation*/
+                               if(increase >= percentIncrease){
+                                console.log(increase);
+                                 if(biddingType === 'MAXIMIZE_CONVERSIONS'){
+                                  binaryAdjustment(audience); 
+                                 }
+                                 if(biddingType === 'TARGET_SPEND'){
+                                  var modifyBid = audience.bidding();
+                                  percentageAdjustment(modifyBid); 
+                                 }
+                                }
+                            }else if (audienceConversions == 0 && audienceClicks > minClicks) {
+                              var difference = audienceCost - cCostConv; 
+                              var increase = (difference/cCostConv) * 100;
+                             /*change to increase >= percentIncrease for live implementation*/
+                               if(increase >= percentIncrease){
+                                console.log(increase);
+                                if(biddingType === 'MAXIMIZE_CONVERSIONS'){                                        
+                                 binaryAdjustment(audience); 
+                                }
+                                if(biddingType === 'TARGET_SPEND'){        
+                                 var modifyBid = audience.bidding();
+                                 percentageAdjustment(modifyBid); 
+                                }
+                               }    
+                            } /*audience conversions = 0 loop */
+                           }/*adgroup audience iterator*/
+                       
+                   } /match campaign and adgroup campaign names/
+                  } /*if active adgroup loop*/ 
+           } /adgroup iterator loop/
+      }/*if no campaign audiences, go to adgroup audiences loop */
+  } /*if active campaign & base campaign loop*/
+  } /*campaign iterator*/     
+} /main loop/
